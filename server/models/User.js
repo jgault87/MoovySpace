6b4a2bab0e48268aeed5d521ba96dfc4277bace1
Marcus mbp <maherreraiv1987@gmail.com>
2022-07-23 17:45:57 -0600

    Movie.js model added

--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,46 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
+const movieSchema = require('./Movie');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, 'Must match an email address!'],
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 5,
-  },
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought',
-    },
-  ],
-});
+const userSchema = new Schema(
+	{
+		// username: {
+		//   type: String,
+		//   required: true,
+		//   unique: true,
+		//   trim: true,
+		// },
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			match: [/.+@.+\..+/, 'Must match an email address!']
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 5
+		},
+		savedMovies: [movieSchema]
+	},
+	{
+		toJSON: {
+			virtuals: true
+		}
+	}
+);
 
 userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+	if (this.isNew || this.isModified('password')) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
 
-  next();
+	next();
 });
 
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+	return bcrypt.compare(password, this.password);
 };
 
 const User = model('User', userSchema);
