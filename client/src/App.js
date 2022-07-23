8a6661bf2477439946e11e1f78787e030d873d52
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-23 14:58:01 -0600

    gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -9,12 +9,8 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Welcome from './components/HomePage/HomePage';
 
 // Construct our main GraphQL API endpoint
