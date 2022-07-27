44d02136009ebbc576010805665dd534b6f684c0
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-26 20:25:00 -0600

    added useEffect, might delete it though

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -8,6 +8,15 @@ const SearchResults = () => {
   const [results, setResults] = useState([]);
 
   // Get search results and set state
+  const searchMovie = async (query) => {
+    const response = await search(query);
+    setResults(response.data.data);
+  };
+
+  // Run this method as component loads so we can render results right away
+  useEffect(() => {
+    searchMovie("12 Pound Balls");
+  }, []);
 
   return <div>SearchResults</div>;
 };
