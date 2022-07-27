7f6984ff2a7ea41e7fc4be76acb9c0a4fcabf4ad
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-26 21:08:05 -0600

    trying to get the trailer API search going using the movie search results

--- client/src/components/SearchResults/SearchResults.js
+++ client/src/components/SearchResults/SearchResults.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function SearchResults(props) {
-  return;
+  return (
+    <ul>
+      {/* {props.results.map((result) => (
+        <li key={result.id}>
+          <img alt={result.title} src={result.images.original.url} />
+        </li>
+      ))} */}
+    </ul>
+  );
 }
 
 export default SearchResults;
--- client/src/components/SearchResults/SearchResultsContainer.js
+++ client/src/components/SearchResults/SearchResultsContainer.js
@@ -2,15 +2,20 @@ import { useState, useEffect } from "react";
 import SearchResults from "./ResultList";
 // Import search method
 import search from "../../utils/API";
+import searchTrailer from "../../utils/TrailerAPI";
 
 const SearchResultsContainer = () => {
   // Declare a new state variable called "results"
   const [results, setResults] = useState([]);
+  const [trailer, setTrailer] = useState([]);
 
   // Get search results and set state
   const searchMovie = async (query) => {
     const response = await search(query);
     setResults(response.data.data);
+    // How do I async line 14 for line 17's await???
+    const trailer = await searchTrailer(response.id);
+    setTrailer(trailer.results[0].key);
   };
 
   // Run this method as component loads so we can render results right away
@@ -21,7 +26,7 @@ const SearchResultsContainer = () => {
 
   return (
     <div>
-      <SearchResults results={results} />
+      <SearchResults results={results} trailer={trailer} />
     </div>
   );
 };
