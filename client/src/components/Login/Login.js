0dd1441bed9b49d0f6f0f2eeb232d23460ec39ec
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-28 14:35:21 -0600

    styling landing page login/signup

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
+import './login.css';
 
 import Auth from '../../utils/auth';
 
@@ -41,11 +42,11 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
+    <main className="">
+      <div className="">
+        <div className="">
+          <h4 className="">Login</h4>
+          <div className="">
             {data ? (
               <p>
                 Success! You may now head{' '}
@@ -70,7 +71,7 @@ const Login = (props) => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn btn-primary"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
