1dc2c5fdfb5ea49d81ced7813cbb8113be5dce5c
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-28 19:29:32 -0600

    seed data added

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,31 +1,17 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
+const { User } = require('../models');
 const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
 
 db.once('open', async () => {
-  try {
-    await Thought.deleteMany({});
-    await User.deleteMany({});
+	try {
+		await User.deleteMany({});
 
-    await User.create(userSeeds);
+		await User.create(userSeeds);
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  console.log('all done!');
-  process.exit(0);
+	console.log('all done!');
+	process.exit(0);
 });
