d7c336a3f9bbc1c09754f06bb78ab43305d55360
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-30 10:49:57 -0600

    removed remaining Thought files from server

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,55 +1,54 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
-  query user($username: String!) {
-    user(username: $username) {
-      _id
-      username
-      email
-      savedMovies {
-        movieId
-        title
-        description
-        image
-        backdrop
-        trailer
-      }
-    }
-  }
+	query user($username: String!) {
+		user(username: $username) {
+			_id
+			username
+			email
+			savedMovies {
+				movieId
+				title
+				description
+				image
+				backdrop
+				trailer
+			}
+		}
+	}
 `;
 
 export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
-      savedMovies {
-        movieId
-        title
-        description
-        image
-        backdrop
-        trailer
-      }
-    }
-  }
-
+	query me {
+		me {
+			_id
+			username
+			email
+			savedMovies {
+				movieId
+				title
+				description
+				image
+				backdrop
+				trailer
+			}
+		}
+	}
 `;
 
 export const QUERY_MOVIE_FEED = gql`
-  query users {
-    users {
-      _id
-      username
-      savedMovies {
-        movieId
-        title
-        description
-        image
-        backdrop
-        trailer
-      }
-    }
-  }
+	query users {
+		users {
+			_id
+			username
+			savedMovies {
+				movieId
+				title
+				description
+				image
+				backdrop
+				trailer
+			}
+		}
+	}
 `;
