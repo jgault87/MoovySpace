9610d79cd5c8b6de9fb3352541a112f932699053
Marcus Herrera <maherreraiv1987@gmail.com>
2022-07-30 19:29:55 -0600

    added code to render users savedMovies to profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
 
 const Profile = () => {
+	// const [savedMovies, setSavedMovies] = useState([])
+
 	const { username: userParam } = useParams();
 
-	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-		variables: { username: userParam }
-	});
+	const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME);
 
 	const user = data?.me || data?.user || {};
+
+	// console.log(user);
+
+	// const movieData = user.savedMovies.map((movie) => ({
+	// 	movieId: movie.movieId,
+	// 	title: movie.title,
+	// 	description: movie.description,
+	// 	image: `https://image.tmdb.org/t/p/w500${movie.image}`,
+	// 	trailer: movie.trailer
+	// }));
+
+	// setSavedMovies(movieData)
+
+	// console.log(movieData);
+
 	// navigate to personal profile page if username is yours
 	if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
 		return <Navigate to="/me" />;
@@ -27,26 +42,37 @@ const Profile = () => {
 
 	return (
 		<div>
-			{/* <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : "your"} profile.
-        </h2>
+			<div className="flex-row justify-center mb-3">
+				<h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
+					Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+				</h2>
 
-        <div className="col-12 col-md-10 mb-5"></div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: "1px dotted #1a1a1a" }}
-          ></div>
-        )}
-      </div> */}
-			<div className="profileContainer">
-				<div className="sidebar">
-					<button>Liked Movies</button>
-					<button>Saved Movies</button>
-					<button>Recommended Movies</button>
-				</div>
-				<div className="carousel">Carousel HERE!</div>
+				<div className="col-12 col-md-10 mb-5"></div>
+				{!userParam && (
+					<div className="profileContainer">
+						<div className="sidebar">
+							<button>Liked Movies</button>
+							<button>Saved Movies</button>
+							<button>Recommended Movies</button>
+						</div>
+						<div className="carousel">
+							{user.savedMovies.map((movie) => {
+								return (
+									<div className="movieCard">
+										<div className="moviePoster">
+											<img
+												alt="movie poster"
+												src={`https://image.tmdb.org/t/p/w500${movie.image}`}
+												width="200px"
+											/>
+										</div>
+										<h1 className="movieTitle">{movie.title}</h1>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	);
