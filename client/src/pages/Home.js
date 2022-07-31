cb99eba2d1d2141017b4892b8a0961362a1bf5b3
Shlomo Siegelman <ssiegelman15@gmail.com>
2022-07-30 19:04:27 -0600

    fixed poster path

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,8 +7,10 @@ import Trailer from "../components/Trailer/Trailer.js";
 const Home = () => {
   const searchContext = useContext(AppContext);
   let posterImage =
-    "https://image.tmdb.org/t/p/w500" + searchContext.details.backdrop_path;
+    "https://image.tmdb.org/t/p/w500" + searchContext.details.poster_path;
   let trailerEmbed = searchContext.trailer.key;
+
+  console.log(searchContext.details);
 
   return (
     <main className="movieContainer">
@@ -20,7 +22,7 @@ const Home = () => {
         <p>{searchContext.details.overview}</p>
         <div className="trailerContainer">
           <Trailer embedId={trailerEmbed} />
-          <p>Trailer</p>
+          {/* <p>Trailer</p> */}
         </div>
         <button id="watchListBtn">Add to watch list</button>
       </div>
