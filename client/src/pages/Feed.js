150522464457ae7bb978a0ca0d7834d12c5fde52
Audra Gault <jgault87@gmail.com>
2022-07-31 23:52:42 -0600

    syling feedpage

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -2,22 +2,24 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_MOVIE_FEED } from '../utils/queries';
 
-
 const feedStyles = {
   gridContainer: {
     display: 'grid',
     gridGap: '3rem',
     padding: '3rem',
-    
   },
   gridItem: {
     position: 'relative',
     backgroundColor: 'var(--color-primary-variant)',
     justifyContent: 'center',
     padding: '1rem',
+    borderRadius: '1rem',
   },
+  gridImage: {
+    borderRadius: '.5rem',
+    
+  }
 };
-
 
 const Feed = () => {
   const { loading, data } = useQuery(QUERY_MOVIE_FEED);
@@ -26,22 +28,26 @@ const Feed = () => {
 
   return (
     <>
-      <h1>Recent Activity:</h1>
+      <h2>Recent Activity:</h2>
       <div style={feedStyles.gridContainer}>
         {loading ? (
           <div>Loading...</div>
         ) : (
           users.map((user) => (
             <div style={feedStyles.gridItem} key={user._id}>
-              <h4>{user.username} recently saved {user.savedMovies.length} movies to their collection</h4>
-  
-              <h3>{user.savedMovies[0].title}</h3>
+              <h4>
+                {user.username} recently saved {user.savedMovies.length} movies
+                to their collection
+              </h4>
+
+              <h4>{user.savedMovies[0].title}</h4>
               <img
+                style={feedStyles.gridImage}
                 src={`https://image.tmdb.org/t/p/w300${user.savedMovies[0].image}`}
                 alt={user.savedMovies[0].title}
               />
-              <p>{user.savedMovies[0].description} Saved Movies</p>
-              
+              {/* <p>{user.savedMovies[0].description} </p> */}
+
               <p>
                 <a
                   href={`https://image.tmdb.org/t/p/original${user.savedMovies[0].backdrop}`}
@@ -52,7 +58,6 @@ const Feed = () => {
               </p>
               <p>{user.savedMovies[0].trailer} </p>
               <p>{user.savedMovies[0].createdAt} </p>
-        
             </div>
           ))
         )}
