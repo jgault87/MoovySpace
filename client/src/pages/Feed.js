5ee8cf1dcef7042c638b8f8ffd484a34883ee4d1
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-07-31 19:56:44 -0600

    added image routes to feed

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -2,10 +2,12 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_MOVIE_FEED } from "../utils/queries";
 
+
 const Feed = () => {
   const { loading, data } = useQuery(QUERY_MOVIE_FEED);
   const users = data?.users || [];
-
+  // console.log(users[0].savedMovies[0].title)
+ 
   return (
     <>
       <h1>Recent Activity:</h1>
@@ -15,10 +17,21 @@ const Feed = () => {
         users.map((user) => (
           <div key={user._id}>
             <h4>{user.username} recently saved:</h4>
-            <p>{user.savedMovies.length} Saved Movies</p>
-            <p>{user.savedMovies[0].title}</p>
-            <p>{user.savedMovies[0].description}</p>
+            <p>{user.savedMovies.length} Movies added to collection</p>
+            <p>{user.savedMovies[0].title} Saved Movies</p>
+            <p>{user.savedMovies[0].description} Saved Movies</p>
+            <img src={`https://image.tmdb.org/t/p/w300/${user.savedMovies[0].image}`} alt={user.savedMovies[0].title}  /> 
+            <p><a href={`https://image.tmdb.org/t/p/original${user.savedMovies[0].backdrop}`} > Backdrop </a> </p>
+            <p>{user.savedMovies[0].trailer} </p>
+            <p>{user.savedMovies[0].createdAt} </p> 
+             
+            
+
+
+          
+            
           </div>
+          
         ))
       )}
     </>
