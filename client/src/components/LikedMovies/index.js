fb7d48a6e74f8df6875fad5e5d2169a0f3c6c5db
Noah Hoffman <stacknoah99@gmail.com>
2022-08-02 17:20:27 -0600

    added test posters and carousel for liked movies page. Going to convert into one page later on

--- client/src/components/LikedMovies/index.js
+++ client/src/components/LikedMovies/index.js
@@ -1,66 +1,63 @@
 import React from 'react';
 import './index.scss';
+import { HashLink } from 'react-router-hash-link'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import img1 from '../../testMoviePosters/test1.jpg';
+import img2 from '../../testMoviePosters/test2.jpg';
+import img3 from '../../testMoviePosters/test3.jpg';
+import img4 from '../../testMoviePosters/test4.jpg';
+import img5 from '../../testMoviePosters/test5.jpg';
+import img6 from '../../testMoviePosters/test6.jpg';
+import img7 from '../../testMoviePosters/test7.jpg';
+import img8 from '../../testMoviePosters/test8.jpg';
+import img9 from '../../testMoviePosters/test9.jpg';
+import img10 from '../../testMoviePosters/test10.jpg';
 
 function LikedMovies() {
-    return (
-        <div class="wrapper">
-            <section id="section1">
-                <a href="#section3">‹</a>
-                <div class="item">
-                    <img src="img1.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img2.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img3.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img4.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img5.jpg" alt="Describe Image" />
-                </div>
-                <a href="#section3">›</a>
-            </section>
-            <section id="section2">
-                <a href="#section1">‹</a>
-                <div class="item">
-                    <img src="img6.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img7.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img8.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img9.jpg" alt="Describe Image" />
-                </div>
-                <div class="item">
-                    <img src="img10.jpg" alt="Describe Image" />
-                </div>
-                <a href="#section3">›</a>
-            </section>
 
-            <section id="section3">
-                <a href="#section2">‹</a>
-                <div class="item">
-                    <img src="img11.jpg" alt="Describe Image" />
+    return (
+        <div className="wrapper">
+            <section id="section1">
+                <a href="#section2"><ArrowBackIosIcon /></a>
+                
+                <div className="item">
+                    <img src={img1} alt="Describe Image" />
                 </div>
-                <div class="item">
-                    <img src="img12.jpg" alt="Describe Image" />
+                <div className="item">
+                    <img src={img2} alt="Describe Image" />
                 </div>
-                <div class="item">
-                    <img src="img13.jpg" alt="Describe Image" />
+                <div className="item">
+                    <img src={img3} alt="Describe Image" />
                 </div>
-                <div class="item">
-                    <img src="img14.jpg" alt="Describe Image" />
+                <div className="item">
+                    <img src={img4} alt="Describe Image" />
                 </div>
-                <div class="item">
-                    <img src="img15.jpg" alt="Describe Image" />
+                <div className="item">
+                    <img src={img5} alt="Describe Image" />
                 </div>
-                <a href="#section1">›</a>
+                <a href="#section2"><ArrowForwardIosIcon /></a>
+            </section>
+            
+            <section id="section2">
+                <a href="#section1"><ArrowBackIosIcon /></a>
+                <div className="item">
+                    <img src={img6} alt="Describe Image" />
+                </div>
+                <div className="item">
+                    <img src={img7} alt="Describe Image" />
+                </div>
+                <div className="item">
+                    <img src={img8} alt="Describe Image" />
+                </div>
+                <div className="item">
+                    <img src={img9} alt="Describe Image" />
+                </div>
+                <div className="item">
+                    <img src={img10} alt="Describe Image" />
+                </div>
+                <a href="#section1"><ArrowForwardIosIcon /></a>
             </section>
         </div>
     )
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,7 +4,7 @@ import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
 import Auth from '../utils/auth';
-import FavoriteMovies from '../components/FavoriteMovies'
+
 
 const Profile = () => {
 	// const [savedMovies, setSavedMovies] = useState([])
@@ -44,8 +44,9 @@ const Profile = () => {
 
 	
 
-
+	console.log(user)
 	return (
+		
 		<div>
 			<div className="flex-row justify-center mb-3">
 				<h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
