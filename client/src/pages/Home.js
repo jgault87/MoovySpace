17aea1731cff41491c8922924c31d7ef2fdcf8ae
Marcus Herrera <maherreraiv1987@gmail.com>
2022-08-03 20:34:24 -0600

    fixed movie data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,89 +1,82 @@
-import React, { useContext, useState } from "react";
-import { useMutation, useQuery } from "@apollo/client";
-import { App, AppContext } from "../App";
-import "./mainPage.css";
-import Trailer from "../components/Trailer/Trailer.js";
-import LikedMovies from "../components/LikedMovies";
-import { SAVE_MOVIE } from "../utils/mutations";
-import Auth from "../utils/auth";
-import { saveMovieIds, getSavedMovieIds } from "../utils/localStorage";
+import React, { useContext, useState } from 'react';
+import { useMutation, useQuery } from '@apollo/client';
+import { App, AppContext } from '../App';
+import './mainPage.css';
+import Trailer from '../components/Trailer/Trailer.js';
+import LikedMovies from '../components/LikedMovies';
+import { SAVE_MOVIE } from '../utils/mutations';
+import Auth from '../utils/auth';
+import { saveMovieIds, getSavedMovieIds } from '../utils/localStorage';
 
 const Home = () => {
-  const [saveMovie] = useMutation(SAVE_MOVIE);
-  const [savedMovieIds, setSavedMovieIds] = useState(getSavedMovieIds());
+	const [saveMovie] = useMutation(SAVE_MOVIE);
+	const [savedMovieIds, setSavedMovieIds] = useState(getSavedMovieIds());
 
-  const searchContext = useContext(AppContext);
-  let posterImage =
-    "https://image.tmdb.org/t/p/w500" + searchContext.details.poster_path;
+	const searchContext = useContext(AppContext);
+	let posterImage = 'https://image.tmdb.org/t/p/w500' + searchContext.details.poster_path;
 
-  const movieId = searchContext.details.id;
-  const movieTitle = searchContext.details.original_title;
-  const movieDescription = searchContext.details.overview;
-  const moviePoster = `https://image.tmdb.org/t/p/w500${searchContext.details.poster_path}`;
-  const movieBackdrop = `https://image.tmdb.org/t/p/w500${searchContext.details.backdrop_path}`;
-  const movieTrailer = `https://www.youtube.com/watch?v=${searchContext.trailer.key}`;
+	const movieId = searchContext.details.id;
+	const movieTitle = searchContext.details.original_title;
+	const movieDescription = searchContext.details.overview;
+	const moviePoster = searchContext.details.poster_path;
+	const movieBackdrop = searchContext.details.backdrop_path;
+	const movieTrailer = searchContext.trailer;
 
-  const movieData = {
-    movieId: movieId,
-    title: movieTitle,
-    description: movieDescription,
-    image: moviePoster,
-    backdrop: movieBackdrop,
-    trailer: movieTrailer,
-  };
+	const movieData = {
+		movieId: movieId,
+		title: movieTitle,
+		description: movieDescription,
+		image: moviePoster,
+		backdrop: movieBackdrop,
+		trailer: movieTrailer
+	};
+	console.log(movieData);
+	const handleSaveMovie = async () => {
+		const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  const handleSaveMovie = async () => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+		if (!token) {
+			return false;
+		}
 
-    if (!token) {
-      return false;
-    }
+		try {
+			await saveMovie({
+				variables: { movie: movieData }
+			});
+			setSavedMovieIds([...savedMovieIds, movieData.movieId]);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-    try {
-      await saveMovie({
-        variables: { movie: movieData },
-      });
-      console.log("Movie Saved");
-      // window.alert('Movie added to your watch list!');
-      setSavedMovieIds([...savedMovieIds, movieData.movieId]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  return (
-    <main className="movieContainer">
-      <div id="moviePoster">
-        <img src={posterImage} alt="Movie Poster" />
-      </div>
-      <div className="movieDetails">
-        <h1>{searchContext.details.title}</h1>
-        <p>{searchContext.details.overview}</p>
-        <div className="trailerContainer">
-          {searchContext.trailer ? (
-            <Trailer className="trailer" embedId={searchContext.trailer} />
-          ) : (
-            <p> VIDEO TRAILER IS NOT AVAILABLE</p>
-          )}
-        </div>
-        {Auth.loggedIn() && (
-          <button
-            disabled={savedMovieIds?.some(
-              (savedMovieId) => savedMovieId === movieData.movieId
-            )}
-            onClick={handleSaveMovie}
-            className="btn btn-primary"
-          >
-            {savedMovieIds?.some(
-              (savedMovieId) => savedMovieId === movieData.movieId
-            )
-              ? "This movie has been add to your Watch List!"
-              : "Add to Watch List"}
-          </button>
-        )}
-      </div>
-    </main>
-  );
+	return (
+		<main className="movieContainer">
+			<div id="moviePoster">
+				<img src={posterImage} alt="Movie Poster" />
+			</div>
+			<div className="movieDetails">
+				<h1>{searchContext.details.title}</h1>
+				<p>{searchContext.details.overview}</p>
+				<div className="trailerContainer">
+					{searchContext.trailer ? (
+						<Trailer className="trailer" embedId={searchContext.trailer} />
+					) : (
+						<p> VIDEO TRAILER IS NOT AVAILABLE</p>
+					)}
+				</div>
+				{Auth.loggedIn() && (
+					<button
+						disabled={savedMovieIds?.some((savedMovieId) => savedMovieId === movieData.movieId)}
+						onClick={handleSaveMovie}
+						className="btn btn-primary"
+					>
+						{savedMovieIds?.some((savedMovieId) => savedMovieId === movieData.movieId)
+							? 'This movie has been add to your Watch List!'
+							: 'Add to Watch List'}
+					</button>
+				)}
+			</div>
+		</main>
+	);
 };
 
 export default Home;
