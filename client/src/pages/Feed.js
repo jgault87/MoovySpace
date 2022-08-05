3ac21cc10a601a941538f07cc58ab9b391715f70
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-08-05 10:37:34 -0600

    sorting to latest movie saved for each user in feed

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import { QUERY_MOVIE_FEED } from "../utils/queries";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_MOVIE_FEED } from '../utils/queries';
+import { Link } from 'react-router-dom';
 
 const feedStyles = {
   gridContainer: {
-    justifyItems: "center",
-    display: "grid",
-    gridGap: "3rem",
-    padding: "3rem",
-    gridTemplateColumns: "1fr",
+    justifyItems: 'center',
+    display: 'grid',
+    gridGap: '3rem',
+    padding: '3rem',
+    gridTemplateColumns: '1fr',
   },
   gridItem: {
-    display: "grid",
-    position: "relative",
-    backgroundColor: "var(--color-bg-variant)",
-    justifyItems: "center",
-    alignItems: "center",
-    padding: "1rem",
-    borderRadius: "1rem",
+    display: 'grid',
+    position: 'relative',
+    backgroundColor: 'var(--color-bg-variant)',
+    justifyItems: 'center',
+    alignItems: 'center',
+    padding: '1rem',
+    borderRadius: '1rem',
   },
   gridImage: {
-    borderRadius: ".5rem",
-    border: "1px solid #ddd",
-    padding: "5px",
-    maxWidth: "50%",
-    height: "auto",
+    borderRadius: '.5rem',
+    border: '1px solid #ddd',
+    padding: '5px',
+    maxWidth: '50%',
+    height: 'auto',
   },
 };
 
@@ -36,7 +36,7 @@ const Feed = () => {
   return (
     <>
       <h2>Recent Activity:</h2>
-      <div className="feedGridContainer" style={feedStyles.gridContainer}>
+      <div className='feedGridContainer' style={feedStyles.gridContainer}>
         {loading ? (
           <div>Loading...</div>
         ) : (
@@ -46,36 +46,41 @@ const Feed = () => {
                 <div style={feedStyles.gridItem} key={user._id}>
                   <h4>
                     <Link
-                      className="btn btn-primary"
+                      className='btn btn-primary'
                       to={`/profiles/${user.username}`}
                     >
                       {user.username}
                     </Link>
                   </h4>
                   <p>
-                    recently saved <strong>{user.savedMovies[0].title}</strong>{" "}
-                    and {user.savedMovies.length - 1} other movies to their
+                    recently saved{' '}
+                    <strong>{user.savedMovies.at(-1).title}</strong> and{' '}
+                    {user.savedMovies.length - 1} other movies to their
                     collection
                   </p>
 
                   <img
                     style={feedStyles.gridImage}
-                    src={`https://image.tmdb.org/t/p/w300${user.savedMovies[0].image}`}
-                    alt={user.savedMovies[0].title}
+                    src={`https://image.tmdb.org/t/p/w300${
+                      user.savedMovies.at(-1).image
+                    }`}
+                    alt={user.savedMovies.at(-1).title}
                   />
 
-                  <h4>{user.savedMovies[0].title}</h4>
+                  <h4>{user.savedMovies.at(-1).title}</h4>
 
                   <a
-                    href={`https://www.youtube.com/watch?v=${user.savedMovies[0].trailer}`}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="btn btn-primary"
+                    href={`https://www.youtube.com/watch?v=${
+                      user.savedMovies.at(-1).trailer
+                    }`}
+                    target='_blank'
+                    rel='noreferrer'
+                    className='btn btn-primary'
                   >
-                    {" "}
-                    Watch the trailer on Youtube!{" "}
+                    {' '}
+                    Watch the trailer on Youtube!{' '}
                   </a>
-                  <p>{user.savedMovies[0].createdAt} </p>
+                  <p>{user.savedMovies.at(-1).createdAt} </p>
                 </div>
               );
             }
