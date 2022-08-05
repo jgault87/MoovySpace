ae903e7fa113d62a9f11ce86a8de82a579f5423c
jgault87 <99287734+jgault87@users.noreply.github.com>
2022-08-04 18:08:02 -0600

    added footer

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './footer.css' 
+
+
+const Footer = () => {
+  return (
+    <footer>
+     
+
+      <ul className="permalinks">
+      
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/jj-gault/" target="_blank" rel="noreferrer">About</a>
+        <a href="https://www.merriam-webster.com/dictionary/about" target="_blank" rel="noreferrer">MoovySpace Github</a>
+        <a href="#home" > </a>
+
+      </div>
+
+      <div className="footer__copyright">
+        <small>Contributors</small>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
