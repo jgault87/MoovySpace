6363d1d381accc8835103d14add03f96cf2994bd
Noah Hoffman <stacknoah99@gmail.com>
2022-08-05 14:31:34 -0600

    added loading screen for profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,6 +7,7 @@ import Auth from '../utils/auth';
 import FavoriteMovies from '../components/FavoriteMovies';
 import LikedMovies from '../components/LikedMovies';
 import WatchListMovies from '../components/WatchListMovies';
+import './profile.css'
 
 const Profile = () => {
 	const { username: userParam } = useParams();
@@ -18,7 +19,13 @@ const Profile = () => {
 		return <Navigate to="/profile" />;
 	}
 	if (loading) {
-		return <div>Loading...</div>;
+		return <div class="loader">
+		<div class="loader__filmstrip">
+		</div>
+		<p class="loader__text">
+		  loading
+		</p>
+	  </div>;
 	}
 	if (!user?.username) {
 		return <h4>You need to be logged in to see this. Use the navigation links above to sign up or log in!</h4>;
