c2cf6dc7a9d2ab807899b463e70c893565acd80e
Noah Hoffman <stacknoah99@gmail.com>
2022-08-05 23:49:22 -0600

    styled footer and fixed main page responsive issues

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,21 +1,79 @@
 import React from 'react'
-import './footer.css'
+import PersonIcon from '@mui/icons-material/Person';
+import './footer.scss'
 
+const links = [
+  {
+    name: 'Source Code',
+    link: 'https://github.com/jgault87/MoovySpace',
+    id: 'sourceCode',
+    icon: function () {
+      return <PersonIcon />
+    }
+  },
+  {
+    name: 'Noah Hoffman',
+    link: 'https://github.com/Noah8863',
+    id: 'noah',
+    icon: function () {
+      return <PersonIcon />
+    }
+  },
+  {
+    name: 'John Gault (JJ)',
+    link: 'https://github.com/jgault87',
+    id: 'jj',
+    icon: function () {
+      return <PersonIcon />
+    }
+  },
+  {
+    name: 'Marcus Herrera',
+    link: 'https://github.com/mahiv87',
+    id: 'marcus',
+    icon: function () {
+      return <PersonIcon />
+    }
+  },
+  {
+    name: 'Shlomo Siegelman',
+    link: 'https://github.com/ssiegelman15',
+    id: 'shlomo',
+    icon: function () {
+      return <PersonIcon />
+    }
+  },
+  {
+    name: 'Elias Vasquez',
+    link: 'https://github.com/gokublue007',
+    id: 'elias',
+    icon: function () {
+      return <PersonIcon />
+    }
+  }
+]
 
 const Footer = () => {
   return (
-    <footer>
-      <ul className="permalinks">
-      </ul>
-      <div className="footer__socials">
-        <a href="https://www.merriam-webster.com/dictionary/about" target="_blank" rel="noreferrer">About</a>
-        <a href="https://github.com/jgault87/MoovySpace" target="_blank" rel="noreferrer">MoovySpace Github</a>
-        <a href="#home" > </a>
+    <section id="contact">
+      <div id="contact-info">
+        {/* Mapping over all the different icons/links and creating an anchor tag and inserting the different object values */}
+        <ul className="footWrapper">
+          {links.map((link) => (
+            <a key={link.id} href={link.link} target="_blank" rel="noreferrer" style={{ color: "white" }}>
+              <li className={`icon ${link.id}`}>
+                <span className="tooltip">
+                  {link.name}
+                </span>
+                <span>
+                  {link.icon()}
+                </span>
+              </li>
+            </a>
+          ))}
+        </ul>
       </div>
-      <div className="footer__copyright">
-        <small>Contributors</small>
-      </div>
-    </footer>
+    </section>
   )
 }
 
