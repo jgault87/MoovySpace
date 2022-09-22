db23f28d7bf6236864e30eb121b69ea32dce01e0
Damien Luzzo <damienluzzo33@gmail.com>
2022-09-21 21:02:42 -0500

    add vscode settings and pseudo code file

---
+++ pseudo.js
@@ -0,0 +1,29 @@
+//! TENTATIVE SCHEDULE 
+// 9/26/2022 - By the end of MNF, we should have all code merged and all MVP features done
+// 9/27/2022 - We should have everything tested and all bugs diagnosed by the end of the night
+// 9/28/2022 - By 3:00 PM MST, have all bugs fixed all code merged, functional deployed app
+// 9/28/2022 - At 3:30 PM MST, meet up to do a final demo run through 
+// 9/28/2022 - At 4:00 PM MST, meet up on discord and all join the zoom thing (it's not zoom)
+
+//? (1) Polish the styles
+// less clunky, more concise and refined
+//? (2) Get favorite movies function to work
+// was not working how it was supposed to
+//? (3) auto complete suggestions (bloodhound thing) needs improvement
+// talk to Shlomo
+//? (4) Be able to add top 3 favorite movies as part of the signup process 
+// Based on those 3 movies, we can give them recommendations on what other movies they should watch
+//? (5) Add a ranking system (our own version of Rotten Tomatoes rankings)
+// MOOVY STARS -- 10 point ranking
+//? (6) Sign up for notifications
+// email notifications based off movies that you like
+// research how to schedule events with node
+//? (7) Be able to actually follow people
+// and let people follow you, see list of people you follow, see movies they like that you don't as suggestions as well?
+//? (8) DUMMY ACCOUNT LOGIN BUTTON (AUTO LOGIN WITH DEFAULT USER)
+// maybe default to seed data stuff each login just for this user
+
+//! POST-MVP (if done with everything by Monday)
+//? Timeline of stuff that your friends are doing
+// likes, ratings, add to lists, all with timestamps and usernames (kinda like a blog!)
+// include friend comments about movie (max 80)
