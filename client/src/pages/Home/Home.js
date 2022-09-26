c573cf524792322a43133b54d71f7773cb5a3e61
Noah Hoffman <stacknoah99@gmail.com>
2022-09-26 16:59:33 -0600

    added and tweaked loading screen for each page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,147 +1,168 @@
-import React, { useContext, useState } from 'react';
-import { useMutation } from '@apollo/client';
-import { AppContext } from '../../App';
-import './mainPage.scss';
-import Trailer from '../../components/Trailer/Trailer.js';
-import { LIKE_MOVIE, SAVE_MOVIE } from '../../utils/mutations';
-import { QUERY_ME } from '../../utils/queries';
-import Auth from '../../utils/auth';
-import { getSavedMovieIds, getLikedMovieIds } from '../../utils/localStorage';
+import React, { useContext, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { useMutation } from "@apollo/client";
+import { AppContext } from "../../App";
+import "./mainPage.scss";
+import Trailer from "../../components/Trailer/Trailer.js";
+import { LIKE_MOVIE, SAVE_MOVIE } from "../../utils/mutations";
+import { QUERY_MOVIE_FEED } from "../../utils/queries";
+import { QUERY_ME } from "../../utils/queries";
+import Auth from "../../utils/auth";
+import { getSavedMovieIds, getLikedMovieIds } from "../../utils/localStorage";
 
 const Home = () => {
-	const [saveMovie] = useMutation(SAVE_MOVIE);
-	const [likeMovie] = useMutation(LIKE_MOVIE);
-	const [savedMovieIds, setSavedMovieIds] = useState(getSavedMovieIds());
-	const [likedMovieIds, setLikedMovieIds] = useState(getLikedMovieIds());
+  const { loading, data } = useQuery(QUERY_MOVIE_FEED);
+  const [saveMovie] = useMutation(SAVE_MOVIE);
+  const [likeMovie] = useMutation(LIKE_MOVIE);
+  const [savedMovieIds, setSavedMovieIds] = useState(getSavedMovieIds());
+  const [likedMovieIds, setLikedMovieIds] = useState(getLikedMovieIds());
 
-	const searchContext = useContext(AppContext);
-	let posterImage = 'https://image.tmdb.org/t/p/w500' + searchContext.details.poster_path;
-	let movieBackdrop = 'https://image.tmdb.org/t/p/w500' + searchContext.details.backdrop_path;
+  const searchContext = useContext(AppContext);
+  let posterImage =
+    "https://image.tmdb.org/t/p/w500" + searchContext.details.poster_path;
+  let movieBackdrop =
+    "https://image.tmdb.org/t/p/w500" + searchContext.details.backdrop_path;
 
-	const movieId = searchContext.details.id;
-	const movieTitle = searchContext.details.original_title;
-	const movieDescription = searchContext.details.overview;
-	const moviePoster = searchContext.details.poster_path;
-	const movieTrailer = searchContext.trailer;
+  const movieId = searchContext.details.id;
+  const movieTitle = searchContext.details.original_title;
+  const movieDescription = searchContext.details.overview;
+  const moviePoster = searchContext.details.poster_path;
+  const movieTrailer = searchContext.trailer;
 
-	const movieData = {
-		movieId: movieId,
-		title: movieTitle,
-		description: movieDescription,
-		image: moviePoster,
-		backdrop: movieBackdrop,
-		trailer: movieTrailer
-	};
+  const movieData = {
+    movieId: movieId,
+    title: movieTitle,
+    description: movieDescription,
+    image: moviePoster,
+    backdrop: movieBackdrop,
+    trailer: movieTrailer,
+  };
 
-	const handleSaveMovie = async () => {
-		const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const handleSaveMovie = async () => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-		if (!token) {
-			return false;
-		}
+    if (!token) {
+      return false;
+    }
 
-		try {
-			await saveMovie({
-				variables: { movie: movieData },
-				update(cache, { data: { saveMovie } }) {
-					try {
-						cache.writeQuery({
-							query: QUERY_ME,
-							data: { me: saveMovie }
-						});
-					} catch (err) {
-						console.error(err);
-					}
-				}
-			});
-			setSavedMovieIds([...savedMovieIds, movieData.movieId]);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+    try {
+      await saveMovie({
+        variables: { movie: movieData },
+        update(cache, { data: { saveMovie } }) {
+          try {
+            cache.writeQuery({
+              query: QUERY_ME,
+              data: { me: saveMovie },
+            });
+          } catch (err) {
+            console.error(err);
+          }
+        },
+      });
+      setSavedMovieIds([...savedMovieIds, movieData.movieId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-	const handleLikeMovie = async () => {
-		const token = Auth.loggedIn() ? Auth.getToken() : null;
+  const handleLikeMovie = async () => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-		if (!token) {
-			return false;
-		}
+    if (!token) {
+      return false;
+    }
 
-		try {
-			await likeMovie({
-				variables: { movie: movieData },
-				update(cache, { data: { likeMovie } }) {
-					try {
-						cache.writeQuery({
-							query: QUERY_ME,
-							data: { me: likeMovie }
-						});
-					} catch (err) {
-						console.error(err);
-					}
-				}
-			});
-			setLikedMovieIds([...likedMovieIds, movieData.movieId]);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+    try {
+      await likeMovie({
+        variables: { movie: movieData },
+        update(cache, { data: { likeMovie } }) {
+          try {
+            cache.writeQuery({
+              query: QUERY_ME,
+              data: { me: likeMovie },
+            });
+          } catch (err) {
+            console.error(err);
+          }
+        },
+      });
+      setLikedMovieIds([...likedMovieIds, movieData.movieId]);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-	return (
-		<main className="movieContainer">
-			<div>
-				<div className="movie_card single">
-					<div className="info_section">
-						<div className="movie_header">
-							<img className="locandina" src={posterImage} />
-							<h1 id='movieTitle'>{searchContext.details.title}</h1>
-							<p className="text">
-								{searchContext.details.overview}
-							</p>
-							{Auth.loggedIn() && (
-								<div>
-									<button
-										disabled={savedMovieIds?.some((savedMovieId) => savedMovieId === movieData.movieId)}
-										onClick={handleSaveMovie}
-										className="btn btn-primary"
-									>
-										{savedMovieIds?.some((savedMovieId) => savedMovieId === movieData.movieId)
-											? 'This movie has been add to your Watch List!'
-											: 'Add to Watch List'}
-									</button>
-									<button
-										disabled={likedMovieIds?.some((likedMovieId) => likedMovieId === movieData.movieId)}
-										onClick={handleLikeMovie}
-										className="btn btn-primary"
-									>
-										{likedMovieIds?.some((likedMovieId) => likedMovieId === movieData.movieId)
-											? 'This movie has been liked!'
-											: 'Like Movie'}
-									</button>
-								</div>
-							)}
-						</div>
-						<div className="movie_desc">
-							<div className="trailerContainer">
-								{searchContext.trailer ? (
-									<Trailer className="trailer" embedId={searchContext.trailer} />
-								) : (
-									<p> VIDEO TRAILER IS NOT AVAILABLE</p>
-								)}
-							</div>
-						</div>
-					</div>
-					<img className="blur_back" src={movieBackdrop}></img>
-				</div>
-			</div>
-		</main >
-	);
+  return (
+    <main className="movieContainer">
+      {loading ? (
+        <div className="loader">
+          <div className="loader__filmstrip"></div>
+          <p className="loader__text">loading</p>
+        </div>
+      ) : (
+        <div>
+          <div className="movie_card single">
+            <div className="info_section">
+              <div className="movie_header">
+                <img className="locandina" src={posterImage} />
+                <h1 id="movieTitle">{searchContext.details.title}</h1>
+                <p className="text">{searchContext.details.overview}</p>
+                {Auth.loggedIn() && (
+                  <div>
+                    <button
+                      disabled={savedMovieIds?.some(
+                        (savedMovieId) => savedMovieId === movieData.movieId
+                      )}
+                      onClick={handleSaveMovie}
+                      className="btn btn-primary"
+                    >
+                      {savedMovieIds?.some(
+                        (savedMovieId) => savedMovieId === movieData.movieId
+                      )
+                        ? "This movie has been add to your Watch List!"
+                        : "Add to Watch List"}
+                    </button>
+                    <button
+                      disabled={likedMovieIds?.some(
+                        (likedMovieId) => likedMovieId === movieData.movieId
+                      )}
+                      onClick={handleLikeMovie}
+                      className="btn btn-primary"
+                    >
+                      {likedMovieIds?.some(
+                        (likedMovieId) => likedMovieId === movieData.movieId
+                      )
+                        ? "This movie has been liked!"
+                        : "Like Movie"}
+                    </button>
+                  </div>
+                )}
+              </div>
+              <div className="movie_desc">
+                <div className="trailerContainer">
+                  {searchContext.trailer ? (
+                    <Trailer
+                      className="trailer"
+                      embedId={searchContext.trailer}
+                    />
+                  ) : (
+                    <p> VIDEO TRAILER IS NOT AVAILABLE</p>
+                  )}
+                </div>
+              </div>
+            </div>
+            <img className="blur_back" src={movieBackdrop}></img>
+          </div>
+        </div>
+      )}
+    </main>
+  );
 };
 
 export default Home;
 
-
-{/* <div id="moviePoster">
+{
+  /* <div id="moviePoster">
 				<img src={posterImage} alt="Movie Poster" />
 			</div>
 			<div className="movieDetails">
@@ -153,4 +174,5 @@ export default Home;
 					) : (
 						<p> VIDEO TRAILER IS NOT AVAILABLE</p>
 					)}
-				</div> */}
+				</div> */
+}
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
-import Auth from '../utils/auth';
-import FavoriteMovies from '../components/FavoriteMovies';
-import LikedMovies from '../components/LikedMovies';
-import WatchListMovies from '../components/WatchListMovies';
-import './profile.css';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import FavoriteMovies from "../components/FavoriteMovies";
+import LikedMovies from "../components/LikedMovies";
+import WatchListMovies from "../components/WatchListMovies";
+import "./profile.css";
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -17,13 +17,13 @@ const Profile = () => {
 
   // navigate to personal profile page if username is yours
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to='/profile' />;
+    return <Navigate to="/profile" />;
   }
   if (loading) {
     return (
-      <div className='loader'>
-        <div className='loader__filmstrip'></div>
-        <p className='loader__text'>loading</p>
+      <div className="loader">
+        <div className="loader__filmstrip"></div>
+        <p className="loader__text">loading</p>
       </div>
     );
   }
@@ -36,42 +36,50 @@ const Profile = () => {
     );
   }
 
-  console.log(user)
+  console.log(user);
   return (
     <div>
-      <div className='flex-row justify-center mb-3'>
-        <h2 className='col-12 col-md-10 bg-dark text-light p-3 mb-5'>
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+      <div className="flex-row justify-center mb-3">
+        <h2
+          className="col-12 col-md-10 bg-dark text-light p-3 mb-5"
+          id="profile-header"
+        >
+          {userParam
+            ? `You're on ${user.username}'s profile`
+            : `Your top movie picks`}
         </h2>
+        <div className="col-12 col-md-10 mb-5"></div>
+        <div>
+          {user.favoriteMovies.length > 0 ? (
+            <FavoriteMovies favoriteMovies={user.favoriteMovies} />
+          ) : (
+            <div className="errorMessage">
+              <h2>Add your favorite movies to view them here!</h2>
+            </div>
+          )}
 
-        <div className='col-12 col-md-10 mb-5'></div>
-        
-          <div className=''>
-            {user.favoriteMovies.length > 0 ? (
-              <FavoriteMovies favoriteMovies={user.favoriteMovies} />
-            ) : (
-              <div className='errorMessage'>
-                <h2>Add your favorite movies to view them here!</h2>
-              </div>
-            )}
+          {user.likedMovies.length > 0 ? (
+            <div>
+              <LikedMovies likedMovies={user.likedMovies}></LikedMovies>
+              <h2 className="movie-headers">Liked Movies</h2>
+            </div>
+          ) : (
+            <div className="errorMessage">
+              <h2>Like some movies, you delinquent!</h2>
+            </div>
+          )}
 
-            {user.likedMovies.length > 0 ? (
-              <LikedMovies likedMovies={user.likedMovies} />
-            ) : (
-              <div className='errorMessage'>
-                <h2>Like some movies, you delinquent!</h2>
-              </div>
-            )}
-
-            {/* {user.savedMovies.length > 0 ? (
+          {user.savedMovies.length > 0 ? (
+            <div>
               <WatchListMovies savedMovies={user.savedMovies} />
-            ) : (
-              <div className='errorMessage'>
-                <h2>Watch something, scoundrel!</h2>
-              </div>
-            )} */}
-          </div>
-       
+              <h2 className="movie-headers">Watch Later</h2>
+            </div>
+          ) : (
+            <div className="errorMessage">
+              <h2>Watch something, scoundrel!</h2>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
